60828bc40f1e28906e5a4522b3aca27382d1b4a4
Icefr09 <hj809@nyu.edu>
2018-12-13 19:12:38 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "db_project"
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(__dirname+'/index.html');
+});
+
+router.post('/login', function(req, res, next) {
+  const email = req.body.username;
+  const password = req.body.password;
+  con.connect(function(err) {
+    if (err) throw err;
+    //console.log("Connected!");
+    const query1 = mysql.format(`Select * from user where uemail='?' and upassword=?` ,[email,password]);
+    console.log(query1);
+    con.query(query1, function (err, result, fields) {
+      if (err) throw err;
+      //console.log(result);
+      res.send(result);
+    });
+  });
+});
+
+module.exports = router;
