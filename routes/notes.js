97b93c53edf503c458ac1d4ec38de83a300391fa
Icefr09 <hj809@nyu.edu>
2018-12-16 13:35:11 -0500

    Implemented add-note

--- routes/notes.js
+++ routes/notes.js
@@ -23,5 +23,26 @@ router.get('/', auth.isUser, function(req, res) {
     });
 });
 
+router.post('/add', function(req, res, next) {
+    console.log(req.body);
+    const ntext = req.body.ntext;
+    const nradius = req.body.nradius;
+    const lname = req.body.lname;
+    const llat = req.body.llat;
+    const llong = req.body.llong;
+    const uid = req.user.uid;
+    con.query(`insert into location (lname, llong, llat) values ('${lname}','${llong}','${llat}');`, function (err) {
+        if(err) console.log(err,'insert location error');
+        con.query(`select * from location where lname = '${lname}' and llong = '${llong}' and llat = '${llat}' `, function (err, rows) {
+            if (err) console.log(err,'find location error');
+            const lid = rows[0].lid;
+            con.query(`insert into note (uid, lid, nradius, ntext, npostTime,nallow) values ('${uid}','${lid}','${nradius}','${ntext}',now(), 1); `, function (err) {
+                if (err) console.log(err, 'add note error');
+                else { console.log('add note success');}
+                res.redirect('/notes');
+            });
+        });
+    });
+});
 
 module.exports = router;
